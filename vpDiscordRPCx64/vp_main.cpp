86831ee64db1c441019f0f496a832085c834beb5
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2022-12-23 19:10:03 +0500

    Support for 32-bit version of J.A.C.K (free)

--- vpDiscordRPCx64/vp_main.cpp
+++ vpDiscordRPCx64/vp_main.cpp
@@ -6,6 +6,20 @@
 #include "includes.h"
 
 //-----------------------------------------------------------------------------
+// Initialize Discord RPC, print plugin info
+//-----------------------------------------------------------------------------
+void InitPlugin()
+{
+	printf("========================\n");
+	printf("https://github.com/ScriptedSnark/vpDiscordRPCx64\n");
+	printf("Discord RPC plugin for J.A.C.K by ScriptedSnark\n");
+	printf("Build timestamp: " __TIMESTAMP__ "\n");
+	printf("========================\n");
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)DiscordRPC_MainThread, NULL, NULL, NULL); // Discord RPC
+}
+
+//-----------------------------------------------------------------------------
 // Initialize our J.A.C.K plugin (REVERSE ENGINEERING INTENSIFIES)
 // 
 // Clean implementation of vpMain (x64):
@@ -22,7 +36,8 @@
 //		return 0i64;
 // }
 //-----------------------------------------------------------------------------
-extern "C" long long EXPORT vpMain(unsigned long long* argv, int version)
+#ifdef _M_X64
+extern "C" long long EXPORT vpMain(unsigned long long* argv, int version) // 64-bit implementation
 {
 #ifdef RETAIL
 	if (version != 100)
@@ -36,16 +51,28 @@ extern "C" long long EXPORT vpMain(unsigned long long* argv, int version)
 
 	setlocale(LC_ALL, "C");
 
-	printf("========================\n");
-	printf("https://github.com/ScriptedSnark/vpDiscordRPCx64\n");
-	printf("Discord RPC plugin for J.A.C.K by ScriptedSnark\n");
-	printf("Build timestamp: " __TIMESTAMP__ "\n");
-	printf("========================\n");
-
-	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)DiscordRPC_MainThread, NULL, NULL, NULL); // Discord RPC
+	InitPlugin();
 
 	return 0i64; // from IDA pseudocode
 }
+#else
+extern "C" int EXPORT vpMain(DWORD* Src, int version) // 32-bit implementation
+{
+	if (*Src < 0x2D0u)
+		return -1;
+
+	if (version != 10)
+		return 10;
+
+	//memcpy(&unk_100170D8, Src, *Src);
+
+	setlocale(LC_ALL, "C");
+
+	InitPlugin();
+
+	return 0;
+}
+#endif
 
 DWORD WINAPI DllMain(_In_ void* _DllHandle, _In_ unsigned long _Reason, _In_opt_ void* _Reserved)
 {
